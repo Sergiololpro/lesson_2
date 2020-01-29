5a9ed9bd0dd2375d11f62ca08ec47d183dca711f
Sergiololpro <homerjsimp@yandex.ru>
2020-01-29 06:40:15 +0300

    Fix

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
           left: item.left + 'px',
           top: item.top + 'px',
           background: item.color,
-          zIndex: [ item.hasParent ? 2 : 0 ]
+          zIndex: [ item.parentId ? 2 : 0 ]
         }"
       />
     </div>
@@ -32,30 +32,40 @@ export default {
     buttons: [
       {
         priority: 25,
+        width: 240,
+        height: 150,
         left: 0,
         top: 100,
         block: false
       },
       {
         priority: 28,
+        width: 240,
+        height: 150,
         left: 100,
         top: 300,
         block: false
       },
       {
         priority: 32,
+        width: 240,
+        height: 150,
         left: 500,
         top: 300,
         block: false
       },
       {
         priority: 48,
+        width: 240,
+        height: 150,
         left: 300,
         top: 500,
         block: false
       },
       {
         priority: 77,
+        width: 240,
+        height: 150,
         left: 400,
         top: 100,
         block: false
@@ -67,8 +77,8 @@ export default {
     drag: {
       bind: (el, bin) => {
         let isMove = false,
-            shiftX,
-            shiftY,
+            rangeX,
+            rangeY,
             coordX = bin.value.left,
             coordY = bin.value.top
 
@@ -77,22 +87,25 @@ export default {
             isMove = true
           }
     
-          shiftX = e.pageX - el.offsetLeft
-          shiftY = e.pageY - el.offsetTop
+          rangeX = e.pageX - el.offsetLeft
+          rangeY = e.pageY - el.offsetTop
           el.style.zIndex = 1
-          /* eslint-disable no-console */
-          // console.log(bin.value.left)
           // console.log(bin.arg[0].left)
           /* eslint-disable no-console */
         })
 
         window.addEventListener('mousemove', (e) => {
           if (isMove) {
-            coordX = e.pageX - shiftX
-            coordY = e.pageY - shiftY
+            coordX = e.pageX - rangeX
+            coordY = e.pageY - rangeY
             
             el.style.left = coordX + 'px'
             el.style.top = coordY + 'px'
+
+            if (Number.isInteger(bin.value.childId)) {
+              bin.arg[bin.value.childId].left = coordX
+              bin.arg[bin.value.childId].top = coordY
+            }
           }
         })
 
@@ -105,27 +118,30 @@ export default {
           bin.value.width = 240
           bin.value.height = 150
 
-          if (Number.isInteger(bin.value.hasParent)) {
-            bin.arg[bin.value.hasParent].isParent = false
-            bin.arg[bin.value.hasParent].color = "white"
-            bin.arg[bin.value.hasParent].block = false
-            bin.value.hasParent = false
+
+          if (Number.isInteger(bin.value.parentId)) {
+            bin.arg[bin.value.parentId].childId = false
+            bin.arg[bin.value.parentId].color = "white"
+            bin.arg[bin.value.parentId].block = false
+            bin.value.parentId = false
           }
 
           bin.arg.forEach(function(em, id){
             if (em.priority !=  bin.value.priority) {
               if (
-                (Math.abs(em.left - bin.value.left) - parseInt(el.style.width) / 2) <= 120
-                && (Math.abs(em.top - bin.value.top) - parseInt(el.style.height) / 2) <= 75
-                && !em.hasParent
-                && !em.isParent
+                (Math.abs(em.left - bin.value.left) - parseInt(el.style.width) / 2) <= parseInt(el.style.width) / 2
+                && (Math.abs(em.top - bin.value.top) - parseInt(el.style.height) / 2) <= parseInt(el.style.height) / 2
+                && !em.parentId
+                && !em.childId
+                && !bin.value.block
               ) {
+  
                 bin.value.left = em.left
                 bin.value.top = em.top
                 bin.value.width = 120
                 bin.value.height = 75
-                bin.value.hasParent = id
-                em.isParent = true
+                bin.value.parentId = id
+                em.childId = bin.arg.findIndex(x => x.priority === bin.value.priority)
 
                 if (bin.value.priority < em.priority) {
                   em.color = "green"
